454a7ad8f6b7197ad3a7e3f3776f89f9185eaa01
JeanLLopes <jeanllopes@outlook.com>
2016-12-01 21:52:50 -0200

    Add project files.

---
+++ ControleAcessos/Controllers/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+//Models
+using CadastroUsuario.Models;
+
+namespace ControleAcessos.Controllers
+{
+    //Toda api do tipo webapi funciona com o protocolo Http
+    //E as chamadas são via verbos.
+    //Estes verbos são: Get, Post, Put, Delete, etc.
+    [Authorize]
+    public class UsuarioController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            //1-Estamos listando os usuários
+            var usuarios = UsuarioModel.GetAll();
+            //2-Estamos enviando os usuarios e informando ao cliente 
+            //que o retorno está OK(sucesso!!)
+            return Request.CreateResponse(HttpStatusCode.OK, usuarios);
+        }
+
+        public HttpResponseMessage Post(UsuarioModel usuario)
+        {
+            //1-Estamos adicionando um usuario vindo do front
+            //2-Quando adicionamos, uma lista é carregada no retorno
+            HttpResponseMessage response;
+
+            try
+            {
+                var usuarios = UsuarioModel.Add(usuario);
+                response = Request.CreateResponse(HttpStatusCode.OK, usuarios);
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ ControleAcessos/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+
+//Como temos o namespace Microsoft.Owin
+//O metodo configuration será executado automaticamente por padrão
+using Microsoft.Owin;
+using Owin;
+
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+
+namespace ControleAcessos
+{
+    //A classe startup define o ciclo de vida da requisição
+    //Automaticamente quando o sistema for carregado
+    //a classe statup sera chamada e o metodo configuration
+    //será invocado!!!
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Para que o ciclo de vida da aplication funcione
+            //com  owin e webapi teremos que instalar os seguintes
+            //pacotes:
+
+            //Install-package Microsoft.aspnet.webapi.owin => funcionar com o webapi
+            //Install-package Microsoft.owin.host.systemweb => funcionar com o iis
+            //Install-package Microsoft.owin.cors => habilitar acessos a chamadas remotas de
+                                                    //outros domínios
+
+            //Este é o objeto de configuração
+            var configuration = new HttpConfiguration();
+
+            //1 - configurar rota
+            ConfigurarRota(configuration);
+
+            //2 - configurar o retorno (de pascal para camelCase)
+            ConfigurarFormatoRetorno(configuration);
+
+
+            //5 - salvar as configurações
+            app.UseWebApi(configuration);
+        }
+
+        public void ConfigurarFormatoRetorno(HttpConfiguration configuration)
+        {
+            //1-temos que remover o formato xml para sempre vir json
+            configuration.Formatters.Remove(configuration.Formatters.XmlFormatter);
+
+            //2-O processo de serialização deve ser do tipo camelcase para
+            //estar de acordo com o formato do javascript
+            //serialização => transformar algo em outro
+            //exemplo: transformar uma classe c# em um json
+            var settings = configuration.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        public void ConfigurarRota(HttpConfiguration configuration)
+        {
+
+            //Habilitar o mapeamento das rotas
+            configuration.MapHttpAttributeRoutes();
+
+            //Definir uma rota padrão
+            configuration.Routes.MapHttpRoute(
+                name: "Default",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional});
+        }
+    }
+}
